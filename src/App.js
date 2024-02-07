8ce96510771679750eb5ad543bc3b342104bf0ce
unknown <diogenrif@gmail.com>
2024-02-07 18:33:12 +0100

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import logo from "./logo.mp4";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={logo} alt="logo mp4" />
+      <h1>Phoenix Intérim : Votre Partenaire de Confiance pour la Construction du Futur</h1>
+
+      <h2>Nous sommes les artisans de demain, spécialisés dans la construction de ponts, routes, centrales électriques, et cités modernes. Engagés envers l'excellence, nous promettons qualité, durabilité et fiabilité à chaque projet. Avec nous, chaque construction est une étape vers un avenir meilleur. Contactez-nous dès aujourd'hui pour concrétiser votre vision. Phoenix Intérim - Construisons l'avenir, ensemble.</h2>
     </div>
   );
 }
